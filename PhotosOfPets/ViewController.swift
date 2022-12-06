98dd24a557a0a90f69c92555200f9f4498ecebe4
Ivan-Tarasenko <80888936+Ivan-Tarasenko@users.noreply.github.com>
2022-12-06 21:20:53 +0700

    first commit

---
+++ PhotosOfPets/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PhotosOfPets
+//
+//  Created by Иван Тарасенко on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
